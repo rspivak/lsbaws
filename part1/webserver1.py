518a2b104349e30fb6d9f2cf4ecd4bad216d62a2
Ruslan Spivak <ruslan.spivak@gmail.com>
2019-07-13 16:32:59 -0400

    Updated the server to run under Python3.7+

--- part1/webserver1.py
+++ part1/webserver1.py
@@ -1,3 +1,4 @@
+# Python3.7+
 import socket
 
 HOST, PORT = '', 8888
@@ -6,13 +7,13 @@ listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 listen_socket.bind((HOST, PORT))
 listen_socket.listen(1)
-print 'Serving HTTP on port %s ...' % PORT
+print(f'Serving HTTP on port {PORT} ...')
 while True:
     client_connection, client_address = listen_socket.accept()
-    request = client_connection.recv(1024)
-    print request
+    request_data = client_connection.recv(1024)
+    print(request_data.decode('utf-8'))
 
-    http_response = """\
+    http_response = b"""\
 HTTP/1.1 200 OK
 
 Hello, World!
